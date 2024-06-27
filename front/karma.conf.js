5531d9d9a9b2954d5c90aab6baf8f99b1bbae92e
Hervé <61780905+Herve-D@users.noreply.github.com>
2024-06-28 00:25:28 +0200

    revert karma.conf.js

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -29,8 +29,7 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' },
-        { type: 'lcov' }
+        { type: 'text-summary' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
